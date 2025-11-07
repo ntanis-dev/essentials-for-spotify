5c5940fcf453dcff3aa3bb536baddf935792e849
Ntanis <59679141+ntanis-dev@users.noreply.github.com>
2025-11-07 23:29:00 +0200

    Refactor device selection logic in wrapper.js

--- src/library/wrapper.js
+++ src/library/wrapper.js
@@ -118,39 +118,13 @@ class Wrapper extends EventEmitter {
 
 	async #deviceCall(path, options, deviceId) {
 		if (!deviceId) {
-			if (!this.#lastDevice) {
-				const activeDevices = this.#lastDevices || []
-				
-				if (activeDevices.length > 0) {
-					// Try to find currently active device
-					const activeDevice = activeDevices.find(device => device.is_active)
-					
-					if (activeDevice) {
-						deviceId = activeDevice.id
-					} else {
-						// Prefer personal devices (Computer, Smartphone, Tablet) over speakers/other devices
-						const computerDevice = activeDevices.find(device => 
-							device.type === 'Computer' || device.type === 'Smartphone' || device.type === 'Tablet'
-						)
-						
-						if (computerDevice) {
-							deviceId = computerDevice.id
-						} else {
-							// Fall back to any non-speaker device
-							const nonSpeakerDevice = activeDevices.find(device => device.type !== 'Speaker')
-							if (nonSpeakerDevice) {
-								deviceId = nonSpeakerDevice.id
-							}
-							// If only speakers available or no devices, leave deviceId undefined
-							// and let Spotify choose
-						}
-					}
-				}
-				// If deviceId is still undefined here, the API call will be made without device_id
-				// allowing Spotify to use its default device selection
-			} else {
+			if (this.#lastDevice)
 				throw new constants.NoDeviceError('No device specified.')
-			}
+
+			const activeDevices = this.#lastDevices?.filter(device => device.type !== 'Speaker')
+
+			if (activeDevices.length > 0)
+				this.#setDevices(activeDevices[0], activeDevices)
 		}
 
 		path = `${path}${path.includes('?') ? '&' : '?'}`
@@ -158,11 +132,10 @@ class Wrapper extends EventEmitter {
 		let response = await connector.callSpotifyApi(`${path}${deviceId ? `device_id=${deviceId}` : ''}`, options, [constants.API_NOT_FOUND_RESPONSE, constants.API_EMPTY_RESPONSE])
 
 		if (response === constants.API_NOT_FOUND_RESPONSE) {
-			const activeDevices = this.#lastDevices || []
-			const activeFilteredDevices = activeDevices.filter(device => device.is_active)
+			const activeDevices = this.#lastDevices?.filter(device => device.is_active)
 
-			if (activeFilteredDevices.length > 0) {
-				response = await connector.callSpotifyApi(`${path}device_id=${activeFilteredDevices[0].id}`, options, [constants.API_NOT_FOUND_RESPONSE, constants.API_EMPTY_RESPONSE])
+			if (activeDevices.length > 0) {
+				response = await connector.callSpotifyApi(`${path}device_id=${activeDevices[0].id}`, options, [constants.API_NOT_FOUND_RESPONSE, constants.API_EMPTY_RESPONSE])
 
 				if (response === constants.API_NOT_FOUND_RESPONSE)
 					throw new constants.NoDeviceError('No device available.')
@@ -219,7 +192,7 @@ class Wrapper extends EventEmitter {
 				progress: response.progress_ms
 			} : null)
 
-			this.#setDevices(response?.device.id ?? this.#lastDevice ?? null, (await connector.callSpotifyApi('me/player/devices')).devices)
+			this.#setDevices(response?.device.id ?? this.#lastDevice ?? null, (await connector.callSpotifyApi('me/player/devices')).devices ?? [])
 			this.#setDisallowFlags((response?.actions?.disallows ? Object.keys(response.actions.disallows).filter(flag => response.actions.disallows[flag]) : []).concat(response?.device.supports_volume ? [] : 'volume'))
 			this.#setCurrentlyPlayingType(response?.currently_playing_type || null)
 		} catch (e) {
@@ -550,7 +523,7 @@ class Wrapper extends EventEmitter {
 				})
 			}, [constants.API_EMPTY_RESPONSE])
 
-			this.#setDevices(deviceId, this.#lastDevices || [])
+			this.#setDevices(deviceId, this.#lastDevices)
 			this.#setDisallowFlags(['volume', 'interrupting_playback', 'toggling_shuffle', 'toggling_repeat_context', 'toggling_repeat_track', 'seeking', 'skipping_next', 'skipping_prev'])
 			this.#setVolumePercent(null)
 
